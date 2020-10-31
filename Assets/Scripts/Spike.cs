08ed6552b271a228f754364e1b75d03cb9570afb
LyaShket <dingofleep@gmail.com>
2020-10-31 16:44:20 +0200

    Spike.cs: add variables for delays

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
+	[SerializeField] float startDelay = 1f;
+	[SerializeField] float upDelay = 4f;
+	[SerializeField] float downDelay = 4f;
+	
+
 	void Start()
 	{
 		StartCoroutine("Move");
@@ -11,16 +16,16 @@ public class Spike : MonoBehaviour
 
 	IEnumerator Move()
 	{
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(startDelay);
 		while (true)
 		{
 			transform.Translate(new Vector2(0, -1.44f));
 
-			yield return new WaitForSeconds(4f);
+			yield return new WaitForSeconds(upDelay);
 
 			transform.Translate(new Vector2(0, 1.44f));
 
-			yield return new WaitForSeconds(4f);
+			yield return new WaitForSeconds(downDelay);
 		}
 	}
 }
