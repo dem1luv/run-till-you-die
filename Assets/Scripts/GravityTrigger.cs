ed1781e0341495a422dfb36ec3f57ab432757f54
LyaShket <dingofleep@gmail.com>
2020-10-31 17:06:11 +0200

    GravityTrigger: add script and test logic

---
+++ Assets/Scripts/GravityTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityTrigger : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			Physics2D.gravity = new Vector2(-9.81f, 0);
+		}
+	}
+}
