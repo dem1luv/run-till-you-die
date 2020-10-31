18a90002986aad00b7db788be6f9e5a981c65deb
LyaShket <dingofleep@gmail.com>
2020-10-31 16:41:22 +0200

    Spike.cs: auto-moving

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+	void Start()
+	{
+		StartCoroutine("Move");
+	}
+
+	IEnumerator Move()
+	{
+		yield return new WaitForSeconds(1f);
+		while (true)
+		{
+			transform.Translate(new Vector2(0, -1.44f));
+
+			yield return new WaitForSeconds(4f);
+
+			transform.Translate(new Vector2(0, 1.44f));
+
+			yield return new WaitForSeconds(4f);
+		}
+	}
+}
