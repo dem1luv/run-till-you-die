255a968428113591f654e05a92fa854dfb478429
LyaShket <dingofleep@gmail.com>
2020-11-01 17:13:43 +0200

    feat: Small spike & Trigger object

---
+++ Assets/Scripts/SmallSpikeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallSpikeTrigger : MonoBehaviour
+{
+    [SerializeField] SmallSpike smallSpike;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+			smallSpike.Fall();
+	}
+}
