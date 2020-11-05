f4b1397a5ed98b1cd44551f49a634251c76e99a7
LyaShket <dingofleep@gmail.com>
2020-11-05 18:01:34 +0200

    refactor: optimize MapGeneratorManager file

--- Assets/Scripts/MapGeneratorManager.cs
+++ Assets/Scripts/MapGeneratorManager.cs
@@ -31,6 +31,28 @@ public class MapGeneratorManager : MonoBehaviour
 
 			randomInt = randomInt == -1 ? 1 : Random.Range(0, 2);
 
+			Vector2 gravityTriggerPos = Vector2.zero;
+			GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
+			GravityTrigger gt = gravityTrigger.GetComponent<GravityTrigger>();
+
+			gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
+
+			if (randomInt == 0)
+			{
+				gravityTriggerPos.x += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
+				gravityTriggerPos.x += blockHeight;
+				gravityTriggerPos.y += blockHeight * 1.5f;
+			}
+			else
+			{
+				gravityTriggerPos.x += blockHeight / 1.5f;
+				gravityTriggerPos.y += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
+				gravityTriggerPos.y += blockHeight / 2f;
+				gravityTrigger.transform.Rotate(new Vector3(0, 0, 90f));
+			}
+
+			gravityTrigger.transform.localPosition = gravityTriggerPos;
+
 			if (lastGravityDirection == Vector2.down)
 			{
 				if (randomInt == 0)
@@ -41,15 +63,8 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.x += blockHeight / 2f;
 					instancePosition.y += (chunk.GetBlockLength() - blockHeight) / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.x += blockHeight;
-					gravityTriggerPos.y += blockHeight * 1.5f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.right;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.up;
+					gt.gravityDirection = Vector2.right;
+					gt.playerDirection = Vector2.up;
 
 					chunk.transform.Rotate(new Vector3(0, 0, 90f));
 				}
@@ -61,16 +76,8 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.x += blockHeight / 2f;
 					instancePosition.y -= (chunk.GetBlockLength() - blockHeight) / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += blockHeight / 1.5f;
-					gravityTriggerPos.y += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.y += blockHeight / 2f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.left;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.down;
-					gravityTrigger.transform.Rotate(new Vector3(0, 0, 90f));
+					gt.gravityDirection = Vector2.left;
+					gt.playerDirection = Vector2.down;
 
 					chunk.transform.Rotate(new Vector3(0, 0, -90f));
 				}
@@ -86,15 +93,8 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.y += blockHeight / 2f;
 					instancePosition.y += lastBlockLength / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.x += blockHeight;
-					gravityTriggerPos.y += blockHeight * 1.5f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.up;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.left;
+					gt.gravityDirection = Vector2.up;
+					gt.playerDirection = Vector2.left;
 
 					chunk.transform.Rotate(new Vector3(0, 0, 180f));
 				}
@@ -107,43 +107,13 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.y += blockHeight / 2f;
 					instancePosition.y += lastBlockLength / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += blockHeight / 1.5f;
-					gravityTriggerPos.y += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.y += blockHeight / 2f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.down;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.right;
-					gravityTrigger.transform.Rotate(new Vector3(0, 0, 90f));
+					gt.gravityDirection = Vector2.down;
+					gt.playerDirection = Vector2.right;
 				}
 			}
 			else if (lastGravityDirection == Vector2.left)
 			{
 				if (randomInt == 0)
-				{
-					lastGravityDirection = Vector2.up;
-
-					instancePosition.x += blockHeight / 2f;
-					instancePosition.x -= chunk.GetBlockLength() / 2f;
-					instancePosition.y -= lastBlockLength / 2f;
-					instancePosition.y -= blockHeight / 2f;
-
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += blockHeight / 1.5f;
-					gravityTriggerPos.y += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.y += blockHeight / 2f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.up;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.left;
-					gravityTrigger.transform.Rotate(new Vector3(0, 0, 90f));
-
-					chunk.transform.Rotate(new Vector3(0, 0, 180f));
-				}
-				else
 				{
 					lastGravityDirection = Vector2.down;
 
@@ -152,18 +122,25 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.y -= lastBlockLength / 2f;
 					instancePosition.y -= blockHeight / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.x += blockHeight;
-					gravityTriggerPos.y += blockHeight * 1.5f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.down;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.right;
+					gt.gravityDirection = Vector2.down;
+					gt.playerDirection = Vector2.right;
+				}
+				else
+				{
+					lastGravityDirection = Vector2.up;
+
+					instancePosition.x += blockHeight / 2f;
+					instancePosition.x -= chunk.GetBlockLength() / 2f;
+					instancePosition.y -= lastBlockLength / 2f;
+					instancePosition.y -= blockHeight / 2f;
+
+					gt.gravityDirection = Vector2.up;
+					gt.playerDirection = Vector2.left;
+
+					chunk.transform.Rotate(new Vector3(0, 0, 180f));
 				}
 			}
-			else if (lastGravityDirection == Vector2.up)
+			else
 			{
 				if (randomInt == 0)
 				{
@@ -173,15 +150,8 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.x -= blockHeight / 2f;
 					instancePosition.y -= (chunk.GetBlockLength() - blockHeight) / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.x += blockHeight;
-					gravityTriggerPos.y += blockHeight * 1.5f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.left;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.down;
+					gt.gravityDirection = Vector2.left;
+					gt.playerDirection = Vector2.down;
 
 					chunk.transform.Rotate(new Vector3(0, 0, -90f));
 				}
@@ -193,16 +163,8 @@ public class MapGeneratorManager : MonoBehaviour
 					instancePosition.x -= blockHeight / 2f;
 					instancePosition.y += (chunk.GetBlockLength() - blockHeight) / 2f;
 
-					Vector2 gravityTriggerPos = Vector2.zero;
-					gravityTriggerPos.x -= chunk.GetBlockLength() / 2f;
-					gravityTriggerPos.x += blockHeight / 1.5f;
-					gravityTriggerPos.y += Utils.ConvertBlockScaleToPosition(gravityTriggerPrefab.transform.localScale.x) / 2f;
-					gravityTriggerPos.y += blockHeight / 2f;
-					GameObject gravityTrigger = Instantiate(gravityTriggerPrefab, chunk.transform);
-					gravityTrigger.transform.localPosition = gravityTriggerPos;
-					gravityTrigger.GetComponent<GravityTrigger>().gravityDirection = Vector2.right;
-					gravityTrigger.GetComponent<GravityTrigger>().playerDirection = Vector2.up;
-					gravityTrigger.transform.Rotate(new Vector3(0, 0, 90f));
+					gt.gravityDirection = Vector2.right;
+					gt.playerDirection = Vector2.up;
 
 					chunk.transform.Rotate(new Vector3(0, 0, 90f));
 				}
