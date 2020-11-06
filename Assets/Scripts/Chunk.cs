00ce95360a05be670ba7380b7553efd7cdbabc47
LyaShket <dingofleep@gmail.com>
2020-11-06 19:59:46 +0200

    feat: create spikes generator

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -16,8 +16,21 @@ public class Chunk : MonoBehaviour
 		blockObject.GetComponent<Block>().Generate();
 		Vector2 spikePos = transform.position;
 		spikePos.y += GetBlockHeight() / 2f + 1.42f / 2f;
-		spikePos.x = Random.Range(transform.position.x - GetBlockLength() / 2f + GetBlockHeight() * 2.4f, transform.position.x + GetBlockLength() / 2f - GetBlockHeight() * 2.4f);
-		//Instantiate(spikePrefab, spikePos, Quaternion.identity, transform);
+		float minObjectX = transform.position.x - GetBlockLength() / 2f + GetBlockHeight() * 3f;
+		float maxObjectX = transform.position.x + GetBlockLength() / 2f - GetBlockHeight() * 2.4f;
+		float step = 1.65f;
+		while (maxObjectX >= minObjectX)
+		{
+			int randInt = Random.Range(0, 2);
+			if (randInt == 0) // 50%
+			{
+				minObjectX += step;
+				minObjectX = Random.Range(minObjectX, maxObjectX);
+			}
+			spikePos.x = minObjectX;
+			minObjectX += step;
+			Instantiate(spikePrefab, spikePos, Quaternion.identity, transform);
+		}
 	}
 
 	public float GetBlockLength()
