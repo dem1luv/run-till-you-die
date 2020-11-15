40b0a40d8d59f2f1b2c62718f78604b1250b03b8
LyaShket <dingofleep@gmail.com>
2020-11-15 10:39:34 +0200

    refactor: fix thorn physics

---
+++ Assets/Scripts/Thorn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thorn : MonoBehaviour
+{
+	Rigidbody2D rb;
+	Chunk parentChunk;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		parentChunk = transform.parent.parent.GetComponent<Chunk>();
+
+		if (parentChunk.gravityDirection.x == 0)
+		{
+			rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+			rb.freezeRotation = true;
+		}
+		else
+		{
+			rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+			rb.freezeRotation = true;
+		}
+	}
+	public void Fall()
+	{
+		rb.bodyType = RigidbodyType2D.Dynamic;
+	}
+}
--- Assets/Scripts/ThornTrigger.cs
+++ Assets/Scripts/ThornTrigger.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmallSpikeTrigger : MonoBehaviour
+public class ThornTrigger : MonoBehaviour
 {
-    [SerializeField] SmallSpike smallSpike;
+    [SerializeField] Thorn thorn;
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.tag == "Player")
-			smallSpike.Fall();
+			thorn.Fall();
 	}
 }
